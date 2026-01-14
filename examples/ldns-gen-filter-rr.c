e19f3b437a0eea84eb2ac967aad3b3b2921a1e8d
Triet Huynh <triet403@gmail.com>
2026-01-13 20:04:35 -0500

    refactor to use hash table
    u

--- examples/ldns-gen-filter-rr.c
+++ examples/ldns-gen-filter-rr.c
@@ -8,9 +8,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include "bloom_filter/bloom.h"
+#include "ldns/error.h"
+#include "ldns/host2wire.h"
+#include "ldns/packet.h"
 #include "ldns/rdata.h"
 #include "ldns/rr.h"
 #include "ldns/rr_functions.h"
+#include "ldns/util.h"
 #include "ldns/zone.h"
 
 #include <sys/types.h>
@@ -20,6 +24,10 @@
 #include <unistd.h>
 
 #include <errno.h>
+
+#include "khashl.h"
+
+KHASHL_MAP_INIT(KH_LOCAL, map32_t, map32, uint32_t, ldns_rr_list*, kh_hash_uint32, kh_eq_generic);
 
 char* prog;
 int verbosity = 2;
@@ -102,17 +110,6 @@ ldns_status load_rrsigs(const char* filename, ldns_rr_list** rrsig_list, bool rr
   return LDNS_STATUS_OK;
 }
 
-int compare_exp_date(const void* a, const void* b)
-{
-  ldns_rr* rrsig_a = *(ldns_rr**)a;
-  ldns_rr* rrsig_b = *(ldns_rr**)b;
-
-  int32_t exp_a = (int32_t)ldns_rdf2native_time_t(ldns_rr_rrsig_expiration(rrsig_a));
-  int32_t exp_b = (int32_t)ldns_rdf2native_time_t(ldns_rr_rrsig_expiration(rrsig_b));
-
-  return exp_a - exp_b;
-}
-
 int main(int argc, char* argv[])
 {
 
@@ -120,7 +117,7 @@ int main(int argc, char* argv[])
   ldns_filter_algorithms filter = BLOOM_FILTER;
   double false_positive = 0.2;
   bool rrsig_file = false;
-  int32_t current_time = 0;
+  uint32_t current_time = 0;
   while ((c = getopt(argc, argv, "f:c:b:u:vp:r")) != -1) {
     switch (c) {
     case 'f':
@@ -145,7 +142,7 @@ int main(int argc, char* argv[])
         fprintf(stderr, "Failed to convert time for -c\n");
         exit(EXIT_FAILURE);
       }
-      current_time = (int32_t)t;
+      current_time = (uint32_t)t;
       break;
     }
     case 'b':
@@ -164,7 +161,7 @@ int main(int argc, char* argv[])
   }
 
   if (current_time == 0) {
-    current_time = (int32_t)time(NULL);
+    current_time = (uint32_t)time(NULL);
   }
 
   argc -= optind;
@@ -219,8 +216,8 @@ int main(int argc, char* argv[])
     if (cmp < 0) {
 
       /* rr1 is smaller than rr2, so rr1 is not in zone file 2 (since lists are sorted) */
-      int32_t orig_ttl = ldns_rdf2native_int32(ldns_rr_rrsig_origttl(rr1));
-      int32_t rrsig_exp = (int32_t)ldns_rdf2native_time_t(ldns_rr_rrsig_expiration(rr1));
+      uint32_t orig_ttl = ldns_rdf2native_int32(ldns_rr_rrsig_origttl(rr1));
+      uint32_t rrsig_exp = ldns_rdf2native_int32(ldns_rr_rrsig_expiration(rr1));
 
       if ((current_time + orig_ttl) < rrsig_exp && ((current_time + (2 * 86400)) < rrsig_exp)) {
 #ifdef DEBUG
@@ -272,71 +269,83 @@ int main(int argc, char* argv[])
   ldns_rr_list_deep_free(sigs1);
   ldns_rr_list_deep_free(sigs2);
 
-  qsort(affected_rrsigs->_rrs,
-        ldns_rr_list_rr_count(affected_rrsigs),
-        sizeof(ldns_rr*),
-        compare_exp_date);
+  int absent;
+  map32_t* exp2rr_list = map32_init();
+  khint_t k;
+  for (size_t i = 0; i < ldns_rr_list_rr_count(affected_rrsigs); i++) {
 
-  // split the rrsigs according to their expiration date.
-  size_t count = ldns_rr_list_rr_count(affected_rrsigs);
-  size_t group_start = 0;
-  for (size_t i = 1; i <= count; i++) {
-    bool end_of_group = false;
-    if (i == count) {
-      end_of_group = true;
+    ldns_rr* rrsig = ldns_rr_list_rr(affected_rrsigs, i);
+    uint32_t exp = ldns_rdf2native_int32(ldns_rr_rrsig_expiration(rrsig));
+    uint32_t exp_day = exp / 86400; // Convert to days since epoch
+
+    k = map32_put(exp2rr_list, exp_day, &absent);
+
+    ldns_rr_list* rrsig_list;
+    if (absent) { // key does not exist
+      rrsig_list = kh_val(exp2rr_list, k) = ldns_rr_list_new();
     }
     else {
-      ldns_rr* prev = ldns_rr_list_rr(affected_rrsigs, group_start);
-      ldns_rr* curr = ldns_rr_list_rr(affected_rrsigs, i);
-      ldns_rdf* prev_exp = ldns_rr_rrsig_expiration(prev);
-      ldns_rdf* curr_exp = ldns_rr_rrsig_expiration(curr);
-
-      time_t t_prev = ldns_rdf2native_time_t(prev_exp);
-      time_t t_curr = ldns_rdf2native_time_t(curr_exp);
-      struct tm tm_prev, tm_curr;
-      localtime_r(&t_prev, &tm_prev);
-      localtime_r(&t_curr, &tm_curr);
-
-      if (tm_prev.tm_year != tm_curr.tm_year || tm_prev.tm_mon != tm_curr.tm_mon || tm_prev.tm_mday != tm_curr.tm_mday) {
-        end_of_group = true;
-      }
+      rrsig_list = kh_val(exp2rr_list, k);
     }
 
-    if (end_of_group) {
-      size_t group_size = i - group_start;
-      ldns_rr* representative = ldns_rr_list_rr(affected_rrsigs, i - 1);
-      ldns_rdf* exp = ldns_rr_rrsig_expiration(representative);
-      time_t t = ldns_rdf2native_time_t(exp);
-      char buf[26];
-      strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", localtime(&t));
-      printf("Expiration: %s\n", buf);
-
-      struct bloom bloom;
-
-      size_t bloom_size = (group_size < 1000) ? 1000 : group_size;
-      printf("Num elem: %zu \n", group_size);
-      if (bloom_init2(&bloom, bloom_size, false_positive) != 0) {
-        fprintf(stderr, "Error initializing bloom filter\n");
-        exit(EXIT_FAILURE);
-      }
-
-      for (size_t j = group_start; j < i; j++) {
-        uint8_t* wire = NULL;
-        size_t size = 0;
-        if (ldns_rr2wire(&wire, ldns_rr_list_rr(affected_rrsigs, j), LDNS_SECTION_ANSWER, &size) == LDNS_STATUS_OK) {
-          bloom_add(&bloom, wire, (int)size);
-          LDNS_FREE(wire);
-        }
-      }
-      bloom_print(&bloom);
-      bloom_free(&bloom);
-      printf("\n");
-
-      group_start = i;
-    }
+    ldns_rr_list_push_rr(rrsig_list, rrsig);
   }
 
-  ldns_rr_list_deep_free(affected_rrsigs);
+  kh_foreach(exp2rr_list, k)
+  { // for each expiration date create a bloom filter
+    ldns_rr_list* rrsig_list = kh_val(exp2rr_list, k);
+
+    struct bloom bloom;
+    size_t rrsig_num = ldns_rr_list_rr_count(rrsig_list);
+    size_t bloom_size = (rrsig_num < 1000) ? 1000 : rrsig_num;
+
+    printf("Num rrsig: %zu \n", rrsig_num);
+
+    if (bloom_init2(&bloom, bloom_size, false_positive) != 0) {
+      fprintf(stderr, "Error initializing bloom filter\n");
+      exit(EXIT_FAILURE);
+    }
+
+    // Track the latest expiration time while adding to bloom filter
+    uint32_t max_exp = 0;
+    for (size_t i = 0; i < ldns_rr_list_rr_count(rrsig_list); i++) {
+      ldns_rr* rr = ldns_rr_list_rr(rrsig_list, i);
+
+      // Track max expiration
+      uint32_t exp = ldns_rdf2native_int32(ldns_rr_rrsig_expiration(rr));
+      if (exp > max_exp) {
+        max_exp = exp;
+      }
+
+      // Add to bloom filter
+      uint8_t* wire = NULL;
+      size_t size = 0;
+      if (ldns_rr2wire(&wire, rr, LDNS_SECTION_ANSWER, &size) == LDNS_STATUS_OK) {
+        bloom_add(&bloom, wire, (int)size);
+        LDNS_FREE(wire);
+      }
+    }
+
+    // Print the latest expiration time
+    time_t t_exp = (time_t)max_exp;
+    struct tm tm_exp;
+    char exp_buf[26];
+    localtime_r(&t_exp, &tm_exp);
+    strftime(exp_buf, sizeof(exp_buf), "%Y-%m-%d %H:%M:%S", &tm_exp);
+    printf("expiration date: %s\n", exp_buf);
+
+    bloom_print(&bloom);
+
+    bloom_free(&bloom);
+  }
+
+  ldns_rr_list_free(affected_rrsigs);
+
+  kh_foreach(exp2rr_list, k)
+  {
+    ldns_rr_list_deep_free(kh_val(exp2rr_list, k));
+  }
+  map32_destroy(exp2rr_list);
 
   exit(EXIT_SUCCESS);
 }
